da7cd10cd79e73007d4c2cad68aaa0de1383c1f7
KpoJleBapKa <ars05@ukr.net>
2023-10-05 00:11:27 +0300

    v1.2 addition don't disturb function, repairing activity function

--- newbot.py
+++ newbot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands, tasks
-import datetime  
+import datetime
 
 intents = discord.Intents.default()
 intents.presences = True
@@ -13,54 +13,63 @@ async def on_ready():
     print(f'Бот {bot.user.name} підключився до Discord!')
     update_status.start()
 
-@tasks.loop(minutes=1)
+@tasks.loop(seconds=30)
 async def update_status():
-    channel_id = 1158891013931278416  # Replace with the ID of the channel where the bot will send messages
+    channel_id = 1158891013931278416  # Замініть на ID каналу, де бот буде відправляти повідомлення
     channel = bot.get_channel(channel_id)
     
     if channel:
-        guild_id = 1141311464985083975  # Replace with your server ID
+        guild_id = 1141311464985083975  # Замініть на ID вашого серверу
         guild = bot.get_guild(guild_id)
         
-        # We receive individual statuses of participants
         online_members = []
         idle_members = []
+        dnd_members = []
         offline_members = []
         
         for member in guild.members:
             if member.status == discord.Status.online:
-                if member.activity:
-                    online_members.append(f'**{member.name}** - **online** - грає у **{member.activity.name}**')
+                if any(activity.type == discord.ActivityType.playing for activity in member.activities):
+                    playing_activities = [activity.name for activity in member.activities if activity.type == discord.ActivityType.playing]
+                    online_members.append(f'**{member.name}** - **online** - грає у **{" / ".join(playing_activities)}**')
                 else:
-                    online_members.append(f'**{member.name}** - **online** - не грає ні в що')
+                    online_members.append(f'**{member.name}** - **online** - не грає у жодну гру')
             elif member.status == discord.Status.idle:
-                if member.activity:
-                    idle_members.append(f'**{member.name}** - **idle** - грає у **{member.activity.name}**')
+                if any(activity.type == discord.ActivityType.playing for activity in member.activities):
+                    playing_activities = [activity.name for activity in member.activities if activity.type == discord.ActivityType.playing]
+                    idle_members.append(f'**{member.name}** - **idle** - грає у **{" / ".join(playing_activities)}**')
                 else:
-                    idle_members.append(f'**{member.name}** - **idle**')
+                    idle_members.append(f'**{member.name}** - **idle** - не грає у жодну гру')
             elif member.status == discord.Status.offline:
                 offline_members.append(f'**{member.name}** - **offline**')
+            elif member.status == discord.Status.dnd:
+                if any(activity.type == discord.ActivityType.playing for activity in member.activities):
+                    playing_activities = [activity.name for activity in member.activities if activity.type == discord.ActivityType.playing]
+                    dnd_members.append(f'**{member.name}** - **don\'t disturb** - грає у **{" / ".join(playing_activities)}**')
+                else:
+                    dnd_members.append(f'**{member.name}** - **don\'t disturb** - не грає у жодну гру')
         
         online_message = '\n'.join(online_members)
         idle_message = '\n'.join(idle_members)
+        dnd_message = '\n'.join(dnd_members)
         offline_message = '\n'.join(offline_members)
 
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'|----------- ONLINE -----------|\n{online_message}\n|----------- IDLE -----------|\n{idle_message}\n|----------- OFFLINE -----------|\n{offline_message}\nОновлено: {current_time}'
+        message = f'|----------- ONLINE -----------|\n{online_message}\n|----------- IDLE -----------|\n{idle_message}\n|----------- DON\'T DISTURB -----------|\n{dnd_message}\n|----------- OFFLINE -----------|\n{offline_message}\nОновлено: {current_time}'
         
         if not hasattr(bot, 'status_message'):
             bot.status_message = await channel.send(message)
         else:
             await bot.status_message.edit(content=message)
 
-
 @bot.event
 async def on_member_update(before, after):
-    if before.status != after.status or before.activity != after.activity:
+    if before.status != after.status or before.activities != after.activities:
         await update_status()
 
 # Запуск бота
-bot.run('YOU_TOKEN_HERE')  # Замініть на свій токен бота
+bot.run('YOUR_TOKEN')  # Замініть на свій токен бота
+
 
 
 
