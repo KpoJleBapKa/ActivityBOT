db661fbdfc418297b67ddec3c6794eaedcfb5f10
KpoJleBapKa <ars05@ukr.net>
2023-10-04 12:32:16 +0300

    v1.1 improvement of the activity output system

--- newbot.py
+++ newbot.py
@@ -15,21 +15,35 @@ async def on_ready():
 
 @tasks.loop(minutes=1)
 async def update_status():
-    channel_id = 1158524081742827532  # Замініть на ID каналу, де бот буде відправляти повідомлення
+    channel_id = 1158891013931278416  # Replace with the ID of the channel where the bot will send messages
     channel = bot.get_channel(channel_id)
     
     if channel:
-        guild_id = 1141311464985083975  # Замініть на ID вашого серверу
+        guild_id = 1141311464985083975  # Replace with your server ID
         guild = bot.get_guild(guild_id)
         
-        # Отримуємо окремо статуси учасників
-        online_members = [member for member in guild.members if member.status == discord.Status.online]
-        idle_members = [member for member in guild.members if member.status == discord.Status.idle]
-        offline_members = [member for member in guild.members if member.status == discord.Status.offline]
+        # We receive individual statuses of participants
+        online_members = []
+        idle_members = []
+        offline_members = []
         
-        online_message = '\n'.join([f'**{member.name}** - **online** - грає у **{member.activity.name}**' if member.activity else f'**{member.name}** - **online** - не грає ні в що' for member in online_members])
-        idle_message = '\n'.join([f'**{member.name}** - **idle**' for member in idle_members])
-        offline_message = '\n'.join([f'**{member.name}** - **offline**' for member in offline_members])
+        for member in guild.members:
+            if member.status == discord.Status.online:
+                if member.activity:
+                    online_members.append(f'**{member.name}** - **online** - грає у **{member.activity.name}**')
+                else:
+                    online_members.append(f'**{member.name}** - **online** - не грає ні в що')
+            elif member.status == discord.Status.idle:
+                if member.activity:
+                    idle_members.append(f'**{member.name}** - **idle** - грає у **{member.activity.name}**')
+                else:
+                    idle_members.append(f'**{member.name}** - **idle**')
+            elif member.status == discord.Status.offline:
+                offline_members.append(f'**{member.name}** - **offline**')
+        
+        online_message = '\n'.join(online_members)
+        idle_message = '\n'.join(idle_members)
+        offline_message = '\n'.join(offline_members)
 
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = f'|----------- ONLINE -----------|\n{online_message}\n|----------- IDLE -----------|\n{idle_message}\n|----------- OFFLINE -----------|\n{offline_message}\nОновлено: {current_time}'
@@ -39,16 +53,17 @@ async def update_status():
         else:
             await bot.status_message.edit(content=message)
 
+
 @bot.event
 async def on_member_update(before, after):
     if before.status != after.status or before.activity != after.activity:
         await update_status()
 
 # Запуск бота
-bot.run('MTE1ODUyNTMwMDU3ODE5NzU1Ng.GKd1hG.78HVUIG66f9CUuKNra6ZHwaQt4d0J7bUA3wgUY')  # Замініть на свій токен бота
+bot.run('YOU_TOKEN_HERE')  # Замініть на свій токен бота
 
 
 
 # token - MTE1ODUyNTMwMDU3ODE5NzU1Ng.GKd1hG.78HVUIG66f9CUuKNra6ZHwaQt4d0J7bUA3wgUY
-# id каналу - 1158891013931278416
+# id каналу - 1158891013931278416 
 # id серверу - 1141311464985083975
